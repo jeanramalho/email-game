93eb4c14cfc34a8ec2af57ca21b0dbdf843bb544
jeanramalho <jeanramalho.dev@gmail.com>
2023-06-13 15:39:20 -0300

    feat(script): Ajustando validação de email

--- script/script.js
+++ script/script.js
@@ -1,10 +1,38 @@
 
 let inputEmail = document.getElementById('inputEmail')
+let responseEmail = document.getElementById('responseEmail')
+
+
+function validaEmail() {
+
+    let usuario = inputEmail.value.substring(0, inputEmail.value.indexOf("@"))
+    let dominio = inputEmail.value.substring(inputEmail.value.indexOf("@")+ 1, inputEmail.value.length);
+
+    if ((usuario.length >=1) &&
+    (usuario.length >=3) &&
+    (usuario.search("@")==-1) &&
+    (dominio.search("@")==-1) &&
+    (usuario.search(" ")==-1) &&
+    (dominio.search(" ")==-1) &&
+    (dominio.search(".")!=-1) &&
+    (dominio.indexOf(".") >=1)&&
+    (dominio.lastIndexOf(".") < dominio.length - 1)) {
+        return true
+}
+else{
+    return false
+}
+
+}
+
+
 
 function acessarButton() {
-    if(inputEmail.value == "") {
-        alert("Por favor, insira uma email válido!")
-    } else (
+    console.log(validaEmail())
+
+    if(inputEmail.value == "" && validaEmail() == false) {
+        responseEmail.innerHTML = "<h1>Insira um email válido</h1>"
+    } else if( validaEmail() == true) (
         alert("email válido")
     )
 }
