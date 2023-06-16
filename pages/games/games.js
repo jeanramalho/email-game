37d26135e5f22614e9f06dd10361c0d2489dc211
jeanramalho <jeanramalho.dev@gmail.com>
2023-06-16 12:58:12 -0300

    feat(games): Finaliza ajustes na página

--- pages/games/games.js
+++ pages/games/games.js
@@ -20,7 +20,8 @@ validaUser(email)
 userIcon.addEventListener('click', function(){
     if(userMenu.classList.contains('lista-active')) {
         userMenu.classList.remove('lista-active')
-        userMenu.classList.add('lista-inactive')
+        userMenu.classList.add('lista-inactive')       
+        
     } else if (userMenu.classList.contains('lista-inactive')) {
         userMenu.classList.remove('lista-inactive')
         userMenu.classList.add('lista-active')
